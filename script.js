68ddd4a5dbfc3c85309d881500fe60e30e3b5e22
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2024-10-08 04:33:15 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+
+  const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    redirectToURL(index + 1);
+  });
+});
+
+function redirectToURL(buttonIndex) {
+  const urlMap = {
+    1: 'https://www.example.com/button1-url',
+    2: 'https://www.example.com/button2-url',
+    3: 'https://www.example.com/button3-url',
+    4: 'https://www.example.com/button4-url',
+  };
+
+  const url = urlMap[buttonIndex];
+  if (url) {
+    redirect(url);
+  }
+}
+
+function redirect(url) {
+  window.location.href = url;
+}
+
+    
